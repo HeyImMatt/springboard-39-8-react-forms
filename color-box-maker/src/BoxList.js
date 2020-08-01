c1e9a580110ef4cd0a9dfa126658db64a99719a1
heyimmatt <mrmcfarland@gmail.com>
2020-07-31 21:22:48 -0700

    Move form to NewBodForm component

--- color-box-maker/src/BoxList.js
+++ color-box-maker/src/BoxList.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import NewBoxForm from './NewBoxForm'
 import Box from './Box';
-import './BoxList.css'
 
 function BoxList() {
   const INITIAL_STATE = {
@@ -29,33 +29,7 @@ function BoxList() {
     <div className="box-form">
       <h3>Color Boxes!</h3>
       <p>Enter box parameters to generate a box</p>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="width">Box Width (in pixels):</label>
-        <input
-          type="number"
-          id="width"
-          name="width"
-          value={formData.width}
-          onChange={changeHandler}
-        />
-        <label htmlFor="height">Box Height (in pixels):</label>
-        <input
-          type="number"
-          id="height"
-          name="height"
-          value={formData.height}
-          onChange={changeHandler}
-        />
-        <label htmlFor="backgroundColor">Box Background Color(hex code or valid css color name):</label>
-        <input
-          type="text"
-          id="boxBackgroundColor"
-          name="boxBackgroundColor"
-          value={formData.boxBackgroundColor}
-          onChange={changeHandler}
-        />
-        <button>Create Box</button>
-      </form>
+      <NewBoxForm formData={formData} submitHandler={submitHandler} changeHandler={changeHandler} />
       {boxesArr.map( box => <Box width={box.width} height={box.height} boxBackgroundColor={box.boxBackgroundColor} />)}
     </div>
   );
