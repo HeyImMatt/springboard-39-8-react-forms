99b146bbdbf865a3a7e8a5fa0478852b0537eb5a
heyimmatt <mrmcfarland@gmail.com>
2020-07-31 18:34:32 -0700

    Add box css

--- color-box-maker/src/Box.js
+++ color-box-maker/src/Box.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Box.css'
 
 function Box({width, height, boxBackgroundColor}) {
   return (
     <div 
+    className='Box'
     style={{
       width: `${width}px`,
       height: `${height}px`,
