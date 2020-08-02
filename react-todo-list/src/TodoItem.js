707b5827828675c948f8812a6f0343b932c4fbc1
heyimmatt <mrmcfarland@gmail.com>
2020-08-02 09:42:01 -0700

    Updates delete button text and adds TodoItem component tests

--- react-todo-list/src/TodoItem.js
+++ react-todo-list/src/TodoItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './TodoItem.css';
 
-function TodoItem( { item, idx, completeTodo, deleteTodo } ) {
+function TodoItem( { item = {todo: "Example Todo", isComplete: false}, idx = 1, completeTodo, deleteTodo } ) {
   return (
     <div className="todo-item" id={idx}>
       <button type="button" className="complete-btn" onClick={completeTodo}>{item.isComplete ? 'Mark Incomplete' : 'Mark Complete'}</button>
       <p className={item.isComplete ? 'complete' : 'incomplete'}>{item.todo}</p>
-      <button type="button" className="delete-btn" onClick={deleteTodo}>X</button>
+      <button type="button" className="delete-btn" onClick={deleteTodo}>Remove X</button>
     </div>
   )
 }
