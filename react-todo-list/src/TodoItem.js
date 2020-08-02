f0a99d52e5dbe4238119db7a76c27dfe166c0ed7
heyimmatt <mrmcfarland@gmail.com>
2020-08-01 23:54:59 -0700

    Add strikethrough for todo item

--- react-todo-list/src/TodoItem.js
+++ react-todo-list/src/TodoItem.js
@@ -4,8 +4,8 @@ import './TodoItem.css';
 function TodoItem( { item, idx, completeTodo, deleteTodo } ) {
   return (
     <div className="todo-item" id={idx}>
-      <button type="button" className="complete-btn" onClick={completeTodo}>{item.isComplete ? 'Incomplete' : 'Complete'}</button>
-      <p>{item.todo}</p>
+      <button type="button" className="complete-btn" onClick={completeTodo}>{item.isComplete ? 'Mark Incomplete' : 'Mark Complete'}</button>
+      <p className={item.isComplete ? 'complete' : 'incomplete'}>{item.todo}</p>
       <button type="button" className="delete-btn" onClick={deleteTodo}>X</button>
     </div>
   )
