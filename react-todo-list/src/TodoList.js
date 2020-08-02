c8357320e3f3ff61dc6e6c97bda81b4e2c3b1d4e
heyimmatt <mrmcfarland@gmail.com>
2020-08-01 22:51:18 -0700

    Add todoList and formData to state, add changeHandler and submitHandler functions

--- react-todo-list/src/TodoList.js
+++ react-todo-list/src/TodoList.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TodoList() {
+  const INITIAL_FORM_DATA = {
+    todo: '',
+    isComplete: false,
+  }
+  const [todoList, setTodoList] = useState([]);
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA)
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((fData) => ({
+      ...fData, 
+      [name]: value,
+    }))
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodoList((list) => [...list, formData])
+  }
   return(
     <div>Hello</div>
   )
